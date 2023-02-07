55eae39d875b4c9fcc298a190f7897c29d3388b7
Nathan Pedigo <njpedigo@gmail.com>
2023-02-07 07:41:22 -0700

    learned some stuff on js

---
+++ app.js
@@ -0,0 +1,108 @@
+function area (height, width) {
+    return height * width;
+}
+
+// alert(area(8,5));
+
+let greeting = "hello world!";
+let num = 15;
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+let funObj = {name: "John Smith", numfav: 22, namesOfPeople: ["Nathan", "Jacob", "Megan", "Kate"]};
+
+let products = Array();
+products[0] = "eggs";
+products[1] = "milk";
+products[2] = "butter";
+products[3] = "cheese";
+
+function fullName (firstName, lastName) {
+    console.log(firstName, lastName);
+}
+
+// fullName("Nathan", "Pedigo");
+
+/* Challenge 1  -- learning how to make variables with let*/
+
+let favFood = "tacos";
+
+// console.log ('My favorite food is', favFood);
+
+favFood = "ice cream";
+
+// console.log ('No, but really, my favorite food is', favFood);
+
+// Challenge 2 -- can't change constant variables
+
+const myCodingConfidence = "unwavering";
+
+console.log("My confidence in my coding ability is", myCodingConfidence);
+
+myCodingConfidence = "poor";
+
+// Challenge 3 -- Learning different data types
+
+const fName = "Nathan";
+let myAge = 23;
+let loveJavaScript = true;
+let listOfHobbies = null;
+
+console.log("My name is", fName);
+console.log("I am", myAge, "years old");
+console.log("It's", loveJavaScript + ", I love JavaScript");
+console.log("The list of things I hate about JavaScript is", listOfHobbies);
+
+// Challenge 4 -- Strings with different quotes
+
+let mySingleString = 'Single Time\n';
+let myDoubleString = "Double Time\n";
+let myBackString = `Backwards Time`;
+
+console.log(mySingleString, myDoubleString, myBackString);
+
+function fizzBuzz(userNumber) {
+    if (userNumber % 3 == 0 && userNumber % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+
+    else if (userNumber % 5 == 0)
+    {
+        console.log("Fizz");
+    }
+
+    else if (userNumber % 3 == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(userNumber);
+    }
+} 
+
+fizzBuzz();
+
+// Greetings
+
+
+function timeGreetings () {
+    let funNum = document.getElementById('numInput').value;
+    if (funNum < 13 && funNum >= 0) {
+        alert("Good Morning");
+    }
+
+    else if (funNum < 17 && funNum > 12) {
+        alert("Good Afternoon");
+    }
+
+    else if (funNum < 20 && funNum > 16) {
+        alert("Good Evening");
+    }
+
+    else if (funNum < 25 && funNum > 19) {
+        alert("Good Night");
+    }
+
+    else {
+        alert("Please enter a number between 0 and 24");
+    }
+}
+
+timeGreetings();
